ffff953fa397b3d2f1821483ec64014518b5be2d
Ronald Arias <ronald.arias@thoughtworks.com>
2020-08-17 12:55:21 -0500

    Creating visual elements for login page

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,42 @@
 import React from 'react';
 
 const Login = () => {
+  const handleOnChange = () => {};
+
   return (
-    <div>
-      <h1>Vista Login</h1>
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Iniciar Sesión</h1>
+        <form>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Iniciar Sesión"
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
